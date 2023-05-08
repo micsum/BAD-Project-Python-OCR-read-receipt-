05de8331b2fb270235ab507452719eaa227b51e0
mic <micsum@connect.hku.hk>
2023-05-09 00:59:22 +0800

    template.....

--- public/controller/receiptController.ts
+++ public/controller/receiptController.ts
@@ -7,28 +7,34 @@ import { ItemInfo } from "../routes/helper";
 
 export class ReceiptController {
   router = Router();
-  receiptMap: Map<number, { id: number; data: [string[], string[]][] }>;
+  receiptMap: Map<
+    number /*userid*/,
+    { receipt_id: number; data: [string[], string[]][] }
+  >;
   constructor(
     private receiptService: ReceiptService,
     private form: IncomingForm
   ) {
     this.receiptMap = new Map();
     this.router.post("/uploadReceipt", this.uploadReadReceipt);
+    this.router.get("/loadReceiptItems", this.loadReceiptItems);
     //this.router.post("/insertReceiptItems", this.insertReceiptItems);
-    this.router.post("/receiptImage/:id", this.viewReceipt);
+    //this.router.post("/receiptImage/:id", this.viewReceipt);
   }
 
   uploadReadReceipt = async (req: Request, res: Response) => {
     //console.log("test upload");
 
-    // if (
-    //   req.session === undefined ||
-    //   req.session.user === undefined ||
-    //   req.session.user.userID === undefined
-    // ) {
-    //   res.status(401).json({ error: "User Not Found" });
-    //   return;
-    // }
+    //if (
+    //  req.session === undefined ||
+    //  req.session.user === undefined ||
+    //  req.session.user.userID === undefined
+    //) {
+    //  res.status(401).json({ error: "User Not Found" });
+    //  return;
+    //}
+    //let userID = req.session.user.userID;
+
     this.form.parse(req, async (err, fields, files) => {
       if (err) {
         console.error(err);
@@ -53,26 +59,47 @@ export class ReceiptController {
       });
       let response = await toPython.json();
 
-      let userID = req.session.user.userID;
-      let id = await this.receiptService.createReceipt(filename, userID);
-
-      this.receiptMap.set(userID, { id: id, data: response });
-      console.log(this.receiptMap);
-      //console.log(response.data); fetch data from python easyocr
+      //let receipt_id = await this.receiptService.createReceipt(
+      //  filename,
+      //  userID
+      //);
+      //
+      //this.receiptMap.set(userID, {
+      //  receipt_id: receipt_id,
+      //  data: response.data,
+      //});
+      //console.log(this.receiptMap);
+      let responseData = response.data;
+      console.log(response.data); //fetch data from python easyocr
 
       //console.log("filename", filename);
       if (responseData === "undefined") {
         res.json({ success: true, error: "receipt content fail to load" });
       } else {
-        res.json({ success: true, data: responseData });
+        res.json({ success: true, data: responseData, filepath: filepath });
       }
       //console.log(path.join(uploadDir + "/" + filename)); test path name and send to python server
       //this.receiptService.createReceipt(filename, req.session.user.userID); update DB immediately after scan?
     });
   };
 
-  viewReceipt = async (req: Request, res: Response) => {};
+  //viewReceipt = async (req: Request, res: Response) => {};
 
+  loadReceiptItems = async (req: Request, res: Response) => {
+    if (
+      req.session === undefined ||
+      req.session.user === undefined ||
+      req.session.user.userID === undefined
+    ) {
+      res.status(401).json({ error: "User Not Found" });
+      return;
+    }
+    let userID = req.session.user.userID;
+
+    res.json({
+      itemList: this.receiptMap.get(userID),
+    });
+  };
   // todo
   insertReceiptItems = async (req: Request, res: Response) => {
     if (
