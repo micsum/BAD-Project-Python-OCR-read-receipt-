afa03776f3c9f1b71c8e5f6fbd297bc1bb2f5991
mic <micsum@connect.hku.hk>
2023-05-14 17:23:00 +0800

    update notification

--- public/js/topUpCredit.js
+++ public/js/topUpCredit.js
@@ -1,106 +1,41 @@
-const stripe = Stripe('pk_test_51N7XVOJtmaIoojFvS7PLBNHaAdEc2Rf8ViFTEdq3NLTILWrsaqrJ7AXErvrGweacm226KESFkx3yxI5dtx8Y9u6400sRbQ5Z60');
+const stripe = Stripe(
+  "pk_test_51N7XVOJtmaIoojFvS7PLBNHaAdEc2Rf8ViFTEdq3NLTILWrsaqrJ7AXErvrGweacm226KESFkx3yxI5dtx8Y9u6400sRbQ5Z60"
+);
 const elements = stripe.elements();
-const cardElement = elements.create('card', {
-    style: {
-      base: {
-        fontSize: '16px',
-        color: '#32325d',
-      }
-    }
-  });
-cardElement.mount('#card-element');
-const cardErrors = document.getElementById('card-errors');
-const amountInput = document.getElementById('amount');
-const payButton = document.getElementById('submit');
+const cardElement = elements.create("card");
+cardElement.mount("#card-element");
+const amountInput = document.getElementById("amount");
+const topUpForm = document.querySelector("#topUpForm");
+const payForm = document.querySelector("#payForm");
+topUpForm.addEventListener("submit", async function (event) {
+  event.preventDefault();
+  const amount = amountInput.value;
 
+  const { clientSecret } = await fetch("/create-payment-intent", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ amount }),
+  }).then((res) => res.json());
 
-cardElement.on('change', function(event) {
-  if (event.error) {
-    cardErrors.textContent = event.error.message;
-  } else {
-    cardErrors.textContent = '';
-  }
-});
+  topUpForm.CDATA_SECTION_NODE.clientSecret = clientSecret;
 
-payButton.addEventListener('click', function() {
-  payButton.disabled = true;
-  stripe.createPaymentMethod({
-    type: 'card',
-    card: cardElement,
-  }).then(function(result) {
-    if (result.error) {
-      cardErrors.textContent = result.error.message;
-      payButton.disabled = false;
-    } else {
-      fetch('/pay', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          paymentMethodId: result.paymentMethod.id,
-          amount: amountInput.value
-        })
-      }).then(function(result) {
-        return result.json();
-      }).then(function(data) {
-        if (data.requiresAction) {
-          stripe.handleCardAction(data.clientSecret).then(function(result) {
-            if (result.error) {
-              cardErrors.textContent = result.error.message;
-              payButton.disabled = false;
-            } else {
-              fetch('/pay', {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                  paymentIntentId: result.paymentIntent.id
-                })
-              }).then(function(result) {
-                return result.json();
-              }).then(function(data) {
-                handlePaymentResult(data);
-              });
-            }
-          });
-        } else {
-          handlePaymentResult(data);
-        }
-      });
-    }
-  });
-});
+  topUpForm.style.display = "none";
+  payForm.style.display = "block";
 
-function handlePaymentResult(data) {
-  if (data.error) {
-    cardErrors.textContent = data.error;
-    payButton.disabled = false;
-  } else if (data.requiresAction) {
-    stripe.handleCardAction(data.clientSecret).then(function(result) {
-      if (result.error) {
-        cardErrors.textContent = result.error.message;
-        payButton.disabled = false;
-      } else {
-        fetch('/pay', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            paymentIntentId: result.paymentIntent.id
-          })
-        }).then(function(result) {
-          return result.json();
-        }).then(function(data) {
-          handlePaymentResult(data);
-        });
-      }
+  payForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const { paymentMethod, error } = await stripe.createPaymentMethod({
+      type: "card",
+      card: cardElement,
     });
-  } else {
-    alert('Payment succeeded');
-    amountInput.value = '';
-    payButton.disabled = false;
-  }
-}
+    if (error) {
+      // Display an error message if the payment method creation fails
+      const errorElement = document.getElementById("card-errors");
+      errorElement.textContent = error.message;
+    } else {
+      const paymentIntentId = payForm.dataset.clientSecret;
+    }
+  });
+});
