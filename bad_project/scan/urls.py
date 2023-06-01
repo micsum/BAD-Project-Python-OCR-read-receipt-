13f1f333d08a8cf976bcdda959a6f91fd0b67b53
mic <micsum@connect.hku.hk>
2023-06-01 11:58:32 +0800

    python set

--- bad_project/scan/urls.py
+++ bad_project/scan/urls.py
@@ -12,15 +12,20 @@ api = NinjaAPI()
 
 @api.post("/readReceipt")
 def readReceipt(request: HttpRequest):
-    if request.method == "POST":
-        data = json.loads(request.body.decode("utf-8"))
-        filepath = data.get("filepath")
-        # print("filepath", filepath)
-        itemText = receiptToTextContent("../"+filepath)
-        #print("itemText",itemText)
-        response_data = {"data": itemText}
-        return JsonResponse(response_data, status=200)
-    else:
+    try:
+        if request.method == "POST":
+            data = json.loads(request.body.decode("utf-8"))
+            filepath = data.get("filepath")
+            # print("filepath", filepath)
+            itemText = receiptToTextContent(os.path.join("../", filepath))
+            #print("itemText",itemText)
+            response_data = {"data": itemText}
+            return JsonResponse(response_data, status=200)
+        else:
+            error_msg ={"error": "sever error"}
+            return JsonResponse(error_msg, status=500)
+    except Exception as e:
+        print(e)
         error_msg ={"error": "sever error"}
         return JsonResponse(error_msg, status=500)
 
